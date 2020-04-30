077a23f13da4a7321965c1d38da68d89a517cbb0
zxl <764880808@qq.com>
2020-04-30 19:05:26 +0800

    第四次提交

--- src/api/system/user.js
+++ src/api/system/user.js
@@ -1,55 +1,33 @@
-import request from "@/utils/request";
+import {get,post,putJSON,deleted} from "@/utils/request";
 
 // 查询用户列表
 export function listUser(query) {
-  return request({
-    url: "/system/user/list",
-    method: "get",
-    params: query
-  });
+  return get( "/system/user/list", query);
 }
 
 // 查询用户详细
 export function getUser(userId) {
-  return request({
-    url: "/system/user/" + userId,
-    method: "get"
-  });
+  return get(  "/system/user/" + userId, "get");
 }
 
 // 新增用户
 export function addUser(data) {
-  return request({
-    url: "/system/user",
-    method: "post",
-    data: data
-  });
+  return post("/system/user",data);
 }
 
 // 修改用户
 export function updateUser(data) {
-  return request({
-    url: "/system/user",
-    method: "put",
-    data: data
-  });
+  return putJSON( "/system/user",data);
 }
 
 // 删除用户
 export function delUser(userId) {
-  return request({
-    url: "/system/user/" + userId,
-    method: "delete"
-  });
+  return deleted("/system/user/" + userId);
 }
 
 // 导出用户
 export function exportUser(query) {
-  return request({
-    url: "/system/user/export",
-    method: "get",
-    params: query
-  });
+  return get("/system/user/export",query);
 }
 
 // 用户密码重置
@@ -58,11 +36,7 @@ export function resetUserPwd(userId, password) {
     userId,
     password
   };
-  return request({
-    url: "/system/user/resetPwd",
-    method: "put",
-    data: data
-  });
+  return put("/system/user/resetPwd", data);
 }
 
 // 用户状态修改
@@ -71,29 +45,12 @@ export function changeUserStatus(userId, status) {
     userId,
     status
   };
-  return request({
-    url: "/system/user/changeStatus",
-    method: "put",
-    data: data
-  });
+  return put("/system/user/changeStatus",data);
 }
 
-// 查询用户个人信息
-export function getUserProfile() {
-  return request({
-    url: "/system/user/profile",
-    method: "get"
-  });
-}
 
-// 修改用户个人信息
-export function updateUserProfile(data) {
-  return request({
-    url: "/system/user/profile",
-    method: "put",
-    data: data
-  });
-}
+
+
 
 // 用户密码重置
 export function updateUserPwd(oldPassword, newPassword) {
@@ -101,34 +58,15 @@ export function updateUserPwd(oldPassword, newPassword) {
     oldPassword,
     newPassword
   };
-  return request({
-    url: "/system/user/profile/updatePwd",
-    method: "put",
-    params: data
-  });
+  return put("/system/user/profile/updatePwd", data);
 }
 
-// 用户头像上传
-export function uploadAvatar(data) {
-  return request({
-    url: "/system/user/profile/avatar",
-    method: "post",
-    data: data
-  });
-}
+
 //查询用户权限信息
 export function getUserRoles(params) {
-  return request({
-    url: "/system/user/list/roles",
-    method: "get",
-    params
-  });
+  return get("/system/user/list/roles",params);
 }
 //修改用户权限信息
 export function updateUserRoles(data) {
-  return request({
-    url: "/system/user/list/update/roles",
-    method: "put",
-    data: data
-  });
+  return get("/system/user/list/update/roles",data);
 }
