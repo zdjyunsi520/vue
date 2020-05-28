355ad05ea6a9d8fa8719306c6464bcd6b6dc221a
zxl <764880808@qq.com>
2020-05-28 23:29:17 +0800

    asdasd

---
+++ src/views/components/TreeSelect/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <el-col>
+    <el-input :disabled="disabled" :value="this.$refs.tree&&this.$refs.tree
+          .getCheckedNodes(true)
+          .map(v => v[this.showText]).join(',')" placeholder="" @focus="showTree" />
+    <el-drawer :close="handleClose" :modal="modal" :title="title" direction="rtl" :visible.sync="dialogVisible" :show-close='false' center :size="size">
+      <el-scrollbar style="height: 86vh;">
+        <el-tree :default-checked-keys="checkedKeys" ref="tree" :node-key="nodeKey" :default-expand-all="true" :props="props" :data="data" show-checkbox :check-strictly="!mutiple" @check-change="handleCheckChange"></el-tree>
+      </el-scrollbar>
+      <div class="dialog-footer">
+        <el-button type="primary" @click="handleConfirm">确 定</el-button>
+        <el-button @click="handleCancel">取 消</el-button>
+      </div>
+    </el-drawer>
+  </el-col>
+</template>
+
+<script>
+export default {
+  props: {
+    //数据源
+    data: {
+      type: Array
+    },
+    title: {
+      type: String,
+      default: "请选择"
+    },
+    props: {
+      type: Object,
+      default() {
+        return {
+          label: "text",
+          children: "childs"
+        };
+      }
+    },
+    nodeKey: {
+      type: String,
+      default: "id"
+    },
+    //多选
+    mutiple: {
+      type: Boolean,
+      default: true
+    },
+    checkedKeys: {
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    showText: {
+      type: String,
+      default: "text"
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      dialogVisible: true,
+      modal: false,
+      size: "0",
+      oldKey: []
+    };
+  },
+  mounted() {
+    this.dialogVisible = false;
+    this.modal = true;
+    setTimeout(() => {
+      this.size = "300px";
+    }, 1000);
+  },
+  methods: {
+    showTree() {
+      this.dialogVisible = true;
+      this.oldKey = this.$refs.tree.getCheckedNodes(true).map(v => v);
+    },
+    handleCheckChange(node, select) {
+      if (!this.mutiple && select) {
+        this.$refs.tree.setCheckedKeys([node.id]);
+      }
+    },
+    handleClose() {
+      console.log(123);
+      this.$refs.tree.setCheckedKeys(this.oldKey.map(v => v.id));
+      this.$emit("change", this.oldKey);
+      this.dialogVisible = false;
+    },
+    handleConfirm() {
+      this.oldKey = this.$refs.tree.getCheckedNodes(true);
+      this.handleClose();
+    },
+    handleCancel() {
+      this.handleClose();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/repairOrder/repair/components/backFile.vue
+++ src/views/repairOrder/repair/components/backFile.vue
@@ -4,8 +4,8 @@
 
       <el-tabs v-model="activeName" @tab-click="handleClick">
         <el-tab-pane label="故障受理" name="add"></el-tab-pane>
-        <el-tab-pane label="故障抢修" name="repair" v-if="form.Status>1"></el-tab-pane>
-        <el-tab-pane label="故障归档" name="backFile" v-if="form.Status>2"></el-tab-pane>
+        <el-tab-pane label="故障抢修" name="repair" v-if="form1.Status>1"></el-tab-pane>
+        <el-tab-pane label="故障归档" name="backFile" v-if="form1.Status>2"></el-tab-pane>
       </el-tabs>
       <!-- <p class="form-smtitle">{{title}}</p> -->
       <el-scrollbar>
@@ -13,17 +13,15 @@
           <el-row>
             <el-col :span="24" :xs="24">
               <el-col :span="11" :xs="24">
-                <el-form-item label="归档人" prop="receivepersonId">
-                  <el-select v-model="form.receivepersonId" placeholder="">
-                    <el-option v-for="(item,index) in processorIds" :key="index" :label="item.text" :value="item.id"></el-option>
-                  </el-select>
+                <el-form-item label="归档人" prop="FilePersonId">
+                  <TreeSelect :disabled="disabled" showText="text" :mutiple="false" :data="allpatrolusers" @change="handleConfirm" :checkedKeys="processpersonId" />
                 </el-form-item>
               </el-col>
             </el-col>
             <el-col :span="24" :xs="24">
               <el-col :span="11" :xs="24">
-                <el-form-item label="归档时间" prop="receivetime">
-                  <el-date-picker v-model="form.receivetime" type="date" placeholder="请选择时间" value-format="yyyy-MM-dd" format="yyyy-MM-dd"></el-date-picker>
+                <el-form-item label="归档时间" prop="FileTime">
+                  <el-date-picker :disabled="disabled" v-model="form.FileTime" type="date" placeholder="请选择时间" value-format="yyyy-MM-dd" format="yyyy-MM-dd"></el-date-picker>
                 </el-form-item>
               </el-col>
             </el-col>
@@ -34,88 +32,41 @@
       </el-scrollbar>
       <el-col :span="24" :xs="24" class="absolute-bottom">
         <div class="form-footer">
-          <el-button type="primary" icon="el-icon-s-promotion" @click="handleSend" :loading="loading">发 送</el-button>
+          <el-button type="primary" icon="el-icon-s-promotion" @click="handleSubmit" :loading="loading" v-if="form1.Status == 3">发 送</el-button>
           <el-button icon="el-icon-arrow-left" @click="handleOpen(null)">返 回</el-button>
         </div>
       </el-col>
 
-      <el-dialog title="人员选择" :visible.sync="dialogEmployeesVisible" center width="500px">
-        <el-tree :data="processorIds" :props="defaultProps" :check-strictly='true' node-key="id" ref="tree" show-checkbox :highlight-current="true" :default-expand-all="true" @check-change='checkchange' :expand-on-click-node="false"></el-tree>
-        <span slot="footer" class="dialog-footer">
-          <el-button type="primary" @click="handleEmpcheck">确 定</el-button>
-          <el-button @click="dialogAssetsVisible = false">取 消</el-button>
-        </span>
-      </el-dialog>
     </div>
   </div>
 </template>
 
 <script>
-import { add, update } from "@/api/repairOrder";
+import { getInfo, add, senderOrder } from "@/api/repairOrder/backFile";
 import { getTrees, getTenantEmployees } from "@/api/org";
 import { mapGetters } from "vuex";
+import TreeSelect from "@/views/components/TreeSelect";
 export default {
+  components: { TreeSelect },
   data() {
     const rules = {
-      // tenantId: [
-      //   {
-      //     required: true,
-      //     message: "用电单位不能为空",
-      //     trigger: "change"
-      //   }
-      // ]
-      // assetsIds: [
-      //   {
-      //     required: true,
-      //     message: "设备不能为空",
-      //     trigger: "change"
-      //   }
-      // ],
-      // rank: [
-      //   {
-      //     required: true,
-      //     message: "缺陷等级不能为空",
-      //     trigger: "change"
-      //   }
-      // ],
-      // detecterId: [
-      //   {
-      //     required: true,
-      //     message: "发现人不能为空",
-      //     trigger: "blur"
-      //   }
-      // ],
-      // detecttime: [
-      //   {
-      //     required: true,
-      //     message: "发现时间不能为空",
-      //     trigger: "change"
-      //   }
-      // ],
-      // processorId: [
-      //   {
-      //     required: true,
-      //     message: "消缺人不能为空",
-      //     trigger: "change"
-      //   }
-      // ],
-      // processdue: [
-      //   {
-      //     required: true,
-      //     message: "处理期限不能为空",
-      //     trigger: "change"
-      //   }
-      // ],
-      // description: [
-      //   {
-      //     required: true,
-      //     message: "缺陷内容不能为空",
-      //     trigger: "blur"
-      //   }
-      // ]
+      FileTime: [
+        {
+          required: true,
+          message: "请选择时间",
+          trigger: "blur"
+        }
+      ],
+      FilePersonId: [
+        {
+          required: true,
+          message: "请选择归档人"
+        }
+      ]
     };
     return {
       form: {},
+      form1: { Status: 0 },
       rules,
       dialogVisible: false,
       loading: false,
@@ -142,140 +93,79 @@ export default {
       ischange: false,
       count: 0,
       selectAssets: [],
-      activeName: "backFile"
+      activeName: "backFile",
+      processpersonId: []
     };
   },
   computed: {
-    ...mapGetters(["name", "userId", "token"])
+    ...mapGetters(["name", "userId", "token"]),
+    disabled() {
+      return this.form1.Status > 3;
+    }
   },
   created() {
     this.getTenantEmployees();
-    let { data, title, TenantIds, ranks } = this.$route.params;
-    this.title = title;
+    let { data, TenantIds } = this.$route.params;
+    this.form1 = Object.assign({}, data);
     this.TenantIds = TenantIds;
-    this.ranks = ranks;
-    if (data && data.Id) {
-      this.getInfo(data);
-    } else {
-      this.reset(data);
-    }
+    this.getInfo(data);
   },
   methods: {
     // 巡视人员
     getTenantEmployees() {
-      getTenantEmployees({})
-        .then(res => {
-          this.allpatrolusers = res.data;
-          if (this.form.repairtenantid) this.getProcessor();
-        })
-        .catch(error => {
-          console.log(error);
-        });
+      getTenantEmployees({}).then(res => {
+        this.allpatrolusers = res.data;
+      });
     },
-
+    handleConfirm(data) {
+      this.processpersonId = data.map(v => v.id);
+      this.form.FilePersonId = this.processpersonId.join(",");
+    },
     changeTenant() {
       this.ischange = true;
       this.getProcessor();
     },
-    // 消缺人下拉
-    getProcessor() {
-      if (this.ischange) {
-        this.form.chargepersonId = "";
-        this.form.receivepersonId = "";
-      }
-      this.allpatrolusers.forEach(v => {
-        if (v.id == this.form.repairtenantid) {
-          this.processorIds = v.childs;
-        }
-      });
-    },
+
     checkchange(data, checked) {
       if (checked) {
         const target = this.$refs.tree;
         target.setCheckedKeys([data.id]);
       }
     },
-    checkchange1(data, checked, node) {
-      this.count++;
-      if (this.count % 2 === 0) {
-        if (checked) {
-          this.$refs.tree.setCheckedKeys([]);
-          this.$refs.tree.setCheckedKeys([data.id]);
-          this.form.assetsIds = data.id;
-          this.form.assetsIdtext = data.text;
-          this.count = 1;
-          //交叉点击节点
-        } else {
-          this.$refs.tree.setCheckedKeys([]);
-          this.form.assetsIds = "";
-          this.form.assetsIdtext = "";
-          this.count = 0;
-          //点击已经选中的节点，置空
-        }
-      } else {
-        this.form.assetsIds = data.id;
-        this.form.assetsIdtext = data.text;
-        this.count = 1;
-      }
-    },
-
-    //设备选择确定
-    handlecheck() {
-      var arr = this.$refs.tree.getCheckedNodes();
-      if (arr.length) {
-        this.form.assetsIdtext = arr[0].text;
-        this.form.assetsIds = arr[0].id;
-        this.dialogAssetsVisible = false;
-      } else {
-        this.$message.error("请选择一个设备");
-      }
-    },
     //人员选择确定
     handleEmpcheck() {
       this.dialogEmployeesVisible = false;
     },
-
-    // 缺陷关联发现时间
-    changeTime() {
-      var d = new Date(this.form.detecttime);
-      var dtime;
-      if (this.form.rank == 1) {
-        dtime = d.setMonth(d.getMonth() + 6);
-      } else if (this.form.rank == 2) {
-        dtime = d.setMonth(d.getMonth() + 1);
-      } else {
-        dtime = d.setDate(d.getDate() + 1);
-      }
-      this.form.processdue = d;
-    },
-
     // 表单重置
     reset(data) {
-      const now = Date.now();
-      var nowTime = new Date(now);
-      var processdueTime = new Date(now);
-      processdueTime.setMonth(processdueTime.getMonth() + 6);
-      this.form = Object.assign({}, data);
+      this.form = Object.assign(
+        {
+          Id: "",
+          FilePersonId: "",
+          FileTime: ""
+        },
+        data
+      );
     },
     getInfo(data) {
       this.loading = true;
       if (data) {
-        const id = { id: data.Id };
-        if (id) {
-          getInfo(id)
+        this.form1 = data;
+        const { Id } = data;
+
+        if (data.Status > 3) {
+          getInfo({ Id })
             .then(res => {
-              this.reset(data);
-              this.form = res.data;
-              // this.form.StartTime = new Date(res.data.StartTime).getTime();
+              this.reset(res.data);
+              this.processpersonId = this.form.FilePersonId
+                ? this.form.FilePersonId.split(",")
+                : [];
             })
             .finally(v => (this.loading = false));
+        } else {
+          this.loading = false;
+          this.reset({ Id });
         }
-      } else {
-        this.loading = false;
-        this.reset(data);
-        this.$nextTick(() => {
-          this.$refs.form.clearValidate();
-        });
       }
     },
     handleOpen(data) {
@@ -284,12 +174,8 @@ export default {
         params: {}
       });
     },
-    // 回退
-    handleBack() {},
-    // 发送
-    handleSend() {},
     handleClick(a) {
-      const data = {};
+      const data = this.form1;
       this.$router.push({
         name: "/repairOrder/repair/components/" + a.name,
         params: { data }
@@ -297,26 +183,21 @@ export default {
     },
     /** 提交按钮 */
     handleSubmit() {
-      console.log(123);
       this.$refs["form"].validate(valid => {
         if (valid) {
-          //按钮转圈圈
           this.loading = true;
-          let fn;
-          if (this.form.id) fn = update;
-          else fn = add;
-          this.form.AttachmentKey = this.imageUrl.map(v => v.uid).join(",");
+          let fn = add;
           fn(this.form)
             .then(res => {
-              //消息提示
-              this.$message.success(res.msg);
-              //刷新列表
-              this.$emit("getList");
-              //关闭窗口
-              this.handleOpen();
+              let Id = res.data.Id;
+              senderOrder({ Id })
+                .then(r => {
+                  this.$message.success("发送成功");
+                  this.handleOpen();
+                })
+                .catch(e => (this.loading = false));
             })
             .catch(r => {
-              //取消按钮转圈圈
               this.loading = false;
             });
         }
