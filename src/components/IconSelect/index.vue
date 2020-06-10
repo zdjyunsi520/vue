e94674f4e5410cbe8150f078c68a05b86aa717dc
zxl520 <764880808@qq.com>
2020-06-11 01:49:58 +0800

    12313123

--- src/components/IconSelect/index.vue
+++ src/components/IconSelect/index.vue
@@ -1,6 +1,6 @@
 <!-- @author zhengjie -->
 <template>
-  <div class="icon-body">
+    <!-- <div class="icon-body">
     <el-input v-model="name" style="position: relative;" clearable placeholder="请输入图标名称" @clear="filterIcons" @input.native="filterIcons">
       <i slot="suffix" class="el-icon-search el-input__icon" />
     </el-input>
@@ -10,60 +10,91 @@
         <span>{{ item }}</span>
       </div>
     </div>
-  </div>
+  </div> -->
+    <div class="icon-body">
+        <div class="icon-list">
+            <div v-for="(item, index) in iconList" :key="index" @click="selectedIcon(item)" :class="name == item?'on':''">
+                <svg-icon :icon-class="item" />
+
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import icons from './requireIcons'
+import icons from "./requireIcons";
 export default {
-  name: 'IconSelect',
-  data() {
-    return {
-      name: '',
-      iconList: icons
-    }
-  },
-  methods: {
-    filterIcons() {
-      if (this.name) {
-        this.iconList = this.iconList.filter(item => item.includes(this.name))
-      } else {
-        this.iconList = icons
-      }
+    name: "IconSelect",
+    data() {
+        return {
+            name: "",
+            iconList: icons
+        };
     },
-    selectedIcon(name) {
-      this.$emit('selected', name)
-      document.body.click()
-    },
-    reset() {
-      this.name = ''
-      this.iconList = icons
+
+    methods: {
+        filterIcons() {
+            if (this.name) {
+                this.iconList = this.iconList.filter(item =>
+                    item.includes(this.name)
+                );
+            } else {
+                this.iconList = icons;
+            }
+        },
+        selectedIcon1(name) {
+            this.$emit("selected", name);
+            document.body.click();
+        },
+        selectedIcon(name) {
+            this.name = name;
+            this.$emit("selected", name);
+        },
+        reset() {
+            this.name = "";
+            this.iconList = icons;
+        }
     }
-  }
-}
+};
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-  .icon-body {
+.icon-body {
     width: 100%;
     padding: 10px;
     .icon-list {
-      height: 200px;
-      overflow-y: scroll;
-      div {
-        height: 30px;
-        line-height: 30px;
-        margin-bottom: -5px;
-        cursor: pointer;
-        width: 33%;
-        float: left;
-      }
-      span {
-        display: inline-block;
-        vertical-align: -0.15em;
-        fill: currentColor;
-        overflow: hidden;
-      }
+        height: 200px;
+        overflow-y: scroll;
+        div {
+            height: 60px;
+            cursor: pointer;
+            width: 60px;
+            float: left;
+            &.on {
+                color: #006eff;
+                border-color: #006eff;
+            }
+            text-align: center;
+            position: relative;
+            border: 1px solid #ececec;
+            border-radius: 5px;
+            margin: 20px 0 0 20px;
+            color: #666;
+        }
+        span {
+            display: inline-block;
+            vertical-align: -0.15em;
+            fill: currentColor;
+            overflow: hidden;
+        }
+        svg {
+            width: 30px;
+            height: 30px;
+            top: 50%;
+            position: absolute;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
     }
-  }
+}
 </style>
--- src/views/commonManager/module/components/add.vue
+++ src/views/commonManager/module/components/add.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="app-container">
-    <div class="search-box  onlyform-box">
-      <p class="form-smtitle">{{title}} </p>
-      <el-scrollbar>
-        <el-form ref="form" label-position="left" :model="form" :rules="rules" label-width="120px" :inline-message="true" style="width:600px">
-          <el-form-item label="名称" prop="name">
-            <el-input v-model="form.name" placeholder="请输入名称" />
-          </el-form-item>
+    <div class="app-container">
+        <div class="search-box  onlyform-box">
+            <p class="form-smtitle">{{title}} </p>
+            <el-scrollbar>
+                <el-form ref="form" label-position="left" :model="form" :rules="rules" label-width="120px" :inline-message="true" style="width:600px">
+                    <el-form-item label="名称" prop="name">
+                        <el-input v-model="form.name" placeholder="请输入名称" />
+                    </el-form-item>
 
-          <el-form-item label="权限标识" prop="key">
-            <el-input v-model="form.key" placeholder="请输入权限标识" />
-          </el-form-item>
+                    <el-form-item label="权限标识" prop="key">
+                        <el-input v-model="form.key" placeholder="请输入权限标识" />
+                    </el-form-item>
 
-          <el-form-item label="url" prop="url">
-            <el-input v-model="form.url" placeholder="请输入url" />
-          </el-form-item>
+                    <el-form-item label="url" prop="url">
+                        <el-input v-model="form.url" placeholder="请输入url" />
+                    </el-form-item>
 
-          <el-form-item label="组件" prop="component">
-            <el-input v-model="form.component" placeholder="请输入组件" />
-          </el-form-item>
+                    <el-form-item label="组件" prop="component">
+                        <el-input v-model="form.component" placeholder="请输入组件" />
+                    </el-form-item>
 
-          <el-form-item label="排序号" prop="sortindex">
-            <el-input-number v-model="form.sortindex" controls-position="right" :min="0" />
-          </el-form-item>
+                    <el-form-item label="排序号" prop="sortindex">
+                        <el-input-number v-model="form.sortindex" controls-position="right" :min="0" />
+                    </el-form-item>
 
-          <el-form-item label="应用图标">
+                    <!-- <el-form-item label="应用图标">
             <el-popover placement="bottom-start" width="500" trigger="click" @show="$refs['iconSelect'].reset()">
               <IconSelect ref="iconSelect" @selected="selected" />
               <el-input slot="reference" v-model="form.iconurl" placeholder="点击选择图标" readonly>
@@ -32,147 +32,155 @@
                 <i v-else slot="prefix" class="el-icon-search el-input__icon" />
               </el-input>
             </el-popover>
-          </el-form-item>
-        </el-form>
-      </el-scrollbar>
-      <el-col :span="24" :xs='24' class="absolute-bottom">
-        <div class="form-footer">
-          <el-button type="primary" icon="el-icon-check" @click="handleSubmit" :loading="loading">保 存</el-button>
-          <el-button icon="el-icon-arrow-left" @click="handleOpen(null)">返 回</el-button>
-        </div>
-      </el-col>
+          </el-form-item> -->
 
+                    <el-form-item label="应用图标">
+                        <IconSelect ref="iconSelect" @selected="selected" />
+                    </el-form-item>
+                </el-form>
+            </el-scrollbar>
+            <el-col :span="24" :xs='24' class="absolute-bottom">
+                <div class="form-footer">
+                    <el-button type="primary" icon="el-icon-check" @click="handleSubmit" :loading="loading">保 存</el-button>
+                    <el-button icon="el-icon-arrow-left" @click="handleOpen(null)">返 回</el-button>
+                </div>
+            </el-col>
+
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import { add, update } from "@/api/commonManager/module";
 import IconSelect from "@/components/IconSelect";
 export default {
-  components: { IconSelect },
-  data() {
-    const rules = {
-      name: [
-        {
-          required: true,
-          message: "输入有误",
-          trigger: "blur"
-        }
-      ],
-      key: [
-        {
-          required: true,
-          message: "输入有误",
-          trigger: "blur"
-        }
-      ],
-      url: [
-        {
-          required: true,
-          message: "输入有误",
-          trigger: "blur"
-        }
-      ],
+    components: { IconSelect },
+    data() {
+        const rules = {
+            name: [
+                {
+                    required: true,
+                    message: "输入有误",
+                    trigger: "blur"
+                }
+            ],
+            key: [
+                {
+                    required: true,
+                    message: "输入有误",
+                    trigger: "blur"
+                }
+            ],
+            url: [
+                {
+                    required: true,
+                    message: "输入有误",
+                    trigger: "blur"
+                }
+            ],
 
-      sortindex: [
-        {
-          required: true,
-          message: "'输入有误",
-          trigger: "blur"
-        }
-      ],
-      phonenumber: [
-        {
-          pattern: /^1\d{10}$/,
-          message: "请输入正确的手机号码",
-          trigger: "blur"
-        }
-      ]
-    };
-    return {
-      form: {},
-      rules,
-      dialogVisible: false,
-      loading: false,
-      title: "",
-      // 角色选项
-      roleOptions: [],
-      deptType: ""
-    };
-  },
-  created() {
-    let { data, title } = this.$route.params;
-    this.title = title;
-    this.reset(data);
-  },
-  computed: {},
-  methods: {
-    selected(name) {
-      this.form.iconurl = name;
+            sortindex: [
+                {
+                    required: true,
+                    message: "'输入有误",
+                    trigger: "blur"
+                }
+            ],
+            phonenumber: [
+                {
+                    pattern: /^1\d{10}$/,
+                    message: "请输入正确的手机号码",
+                    trigger: "blur"
+                }
+            ]
+        };
+        return {
+            form: {},
+            rules,
+            dialogVisible: false,
+            loading: false,
+            title: "",
+            // 角色选项
+            roleOptions: [],
+            deptType: ""
+        };
     },
-    // 表单重置
-    reset(data) {
-      this.form = Object.assign(
-        {
-          id: "",
-          url: "",
-          name: "",
-          key: "",
-          type: 1,
-          component: "",
-          iconurl: "",
-          sortindex: 1
+    mounted() {
+        let { data, title } = this.$route.params;
+        this.title = title;
+        this.reset(data);
+    },
+
+    computed: {},
+    methods: {
+        selected(name) {
+            this.form.iconurl = name;
         },
-        data
-      );
-    },
-    handleOpen() {
-      const data = this.form;
-      this.$router.push({
-        name: "/commonManager/module/index",
-        params: { data }
-      });
-    },
-    /** 提交按钮 */
-    handleSubmit: function() {
-      this.$refs["form"].validate(valid => {
-        if (valid) {
-          //按钮转圈圈
-          this.loading = true;
-          let fn;
-          if (this.form.id) fn = update;
-          else fn = add;
-          fn(this.form)
-            .then(response => {
-              //消息提示
-              var txt = this.form.id ?'编辑成功！':'新增成功！'
-              this.$message.success(txt);
+        // 表单重置
+        reset(data) {
+            this.form = Object.assign(
+                {
+                    id: "",
+                    url: "",
+                    name: "",
+                    key: "",
+                    type: 1,
+                    component: "",
+                    iconurl: "",
+                    sortindex: 1
+                },
+                data
+            );
+            this.$refs.iconSelect.name = this.form.iconurl;
+        },
+        handleOpen() {
+            const data = this.form;
+            this.$router.push({
+                name: "/commonManager/module/index",
+                params: { data }
+            });
+        },
+        /** 提交按钮 */
+        handleSubmit: function() {
+            this.$refs["form"].validate(valid => {
+                if (valid) {
+                    //按钮转圈圈
+                    this.loading = true;
+                    let fn;
+                    if (this.form.id) fn = update;
+                    else fn = add;
+                    fn(this.form)
+                        .then(response => {
+                            //消息提示
+                            var txt = this.form.id
+                                ? "编辑成功！"
+                                : "新增成功！";
+                            this.$message.success(txt);
 
-              this.form.id = response.data.Id;
-              //关闭窗口
-              this.handleOpen();
-            })
-            .catch(r => {
-              //取消按钮转圈圈
-              this.loading = false;
+                            this.form.id = response.data.Id;
+                            //关闭窗口
+                            this.handleOpen();
+                        })
+                        .catch(r => {
+                            //取消按钮转圈圈
+                            this.loading = false;
+                        });
+                }
             });
         }
-      });
     }
-  }
 };
 </script>
 
 <style lang="scss" scoped>
 /deep/.el-select {
-  width: 100%;
+    width: 100%;
 }
 
 /deep/.el-form-item__error--inline {
-  position: absolute;
-  top: 50%;
-  margin-top: -10px;
-  width: 100%;
+    position: absolute;
+    top: 50%;
+    margin-top: -10px;
+    width: 100%;
 }
 </style>
