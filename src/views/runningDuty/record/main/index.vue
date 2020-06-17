eba6b7cbe404bf3ee42bb20447cd9d2b771fe930
zxl <764880808@qq.com>
2020-06-17 18:16:41 +0800

    123123

--- src/views/runningDuty/record/main/index.vue
+++ src/views/runningDuty/record/main/index.vue
@@ -20,7 +20,7 @@
                     <el-input :value='"岗位："+form.PositionName' disabled />
                   </el-form-item>
                   <el-form-item prop="StartTime">
-                    <el-input :value="'接班时间：'+this.parseTime(form.StartTime, '{y}-{m}-{d} {hh}:{mm}:{ss}')" disabled />
+                    <el-input :value="'接班时间：'+(form.StartTime?this.parseTime(form.StartTime, '{y}-{m}-{d} {hh}:{mm}:{ss}'):'')" disabled />
                   </el-form-item>
                   <el-form-item prop="UserName">
                     <el-input :value="'接班人：'+form.UserName" disabled />
@@ -43,8 +43,8 @@
               </div>
             </el-card>
             <div :class="isDisabled?'databtnbox left':'databtnbox left blue'">
-              <el-button type="primary" :plain='!isDisabled?false:true' circle size="mini">接</el-button>
-              <label>{{startTime}}</label>
+              <el-button type="primary" :plain='isDisabled' circle size="mini">接</el-button>
+              <label>{{startTime.replace(/0:00:00/,'')}}</label>
             </div>
           </div>
           <div class="rightcard">
@@ -53,12 +53,12 @@
                 <span>交班信息</span>
               </div>
               <div class="text item">
-                <el-form ref="form" :model="form1" :rules="rules" :inline="true">
+                <el-form ref="form1" :model="form1" :rules="rules" :inline="true">
                   <el-form-item prop="PositionName">
                     <el-input :value='"岗位："+form1.PositionName' disabled />
                   </el-form-item>
                   <el-form-item prop="StartTime">
-                    <el-input :value="'交班时间：'+ this.parseTime(form1.StartTime, '{y}-{m}-{d} {hh}:{mm}:{ss}')" disabled />
+                    <el-input :value="'交班时间：'+ (form1.StartTime?this.parseTime(form1.StartTime, '{y}-{m}-{d} {hh}:{mm}:{ss}'):'')" disabled />
                   </el-form-item>
                   <el-form-item prop="UserName">
                     <el-input :value="'交班人：'+form1.UserName" disabled />
@@ -74,16 +74,16 @@
                     <el-input v-model="form1.Temperature" placeholder="请输入温度" />
                   </el-form-item>
                   <el-form-item class="cardbtns">
-                    <el-button type="primary" size="mini" @click="handleAccept">交班</el-button>
+                    <el-button type="primary" size="mini" @click="handleAccept1" :disabled='isDisabled1'>交班</el-button>
                     <!-- <el-button type="primary" size="mini" plain>召回</el-button> -->
-                    <el-button type="info" size="mini" plain @click="handleReset1">重置</el-button>
+                    <el-button type="info" size="mini" plain @click="handleReset1" :disabled='isDisabled1'>重置</el-button>
                   </el-form-item>
                 </el-form>
               </div>
             </el-card>
-            <div :class="isDisabled?'databtnbox right blue':'databtnbox right '">
-              <label>{{endTime}}</label>
-              <el-button type="primary" :plain='isDisabled?false:true' circle  size="mini">交</el-button>
+            <div :class="isDisabled1?'databtnbox right blue':'databtnbox right '">
+              <label>{{endTime.replace(/0:00:00/,'')}}</label>
+              <el-button type="primary" :plain='isDisabled1' circle size="mini">交</el-button>
             </div>
           </div>
         </div>
@@ -95,16 +95,16 @@
 
 <script>
 import { mapGetters } from "vuex";
-import { fetchList, deleted } from "@/api/runningDuty/record/shiftRecord";
+import { addHandover, updateHandover } from "@/api/runningDuty/record";
 
 export default {
   name: "user",
   components: {},
   props: {
     currentData: {
-      type: Array,
+      type: Object,
       default() {
-        return [];
+        return {};
       }
     },
     dataTime: {
@@ -113,56 +113,157 @@ export default {
   },
 
   data() {
+    const rules = {
+      Weather: [
+        {
+          required: true,
+          message: "请选择天气",
+          trigger: "change"
+        }
+      ],
+      Temperature: [
+        {
+          required: true,
+          message: "请填写气温",
+          trigger: "blur"
+        }
+      ]
+    };
     return {
-      rules: {},
-      isDisabled: false,
+      rules,
+
       form: {},
       form1: {},
-      startTime:'',
-      endTime:'',
-      
+      startTime: "",
+      endTime: ""
     };
   },
 
-  created() {},
+  mounted() {
+    this.$refs.form.clearValidate();
+    this.$refs.form1.clearValidate();
+  },
   watch: {
     currentData: {
       handler: function(val, oldval) {
-        var newval  = val.model;
-        this.startTime = val.startTime;
-        this.endTime = val.endTime; 
-        this.reset(newval[0]);
-        this.reset1(newval[0]);
-        if (newval.length > 0) {
-          this.isDisabled = true;
-          this.getCurrentDate1();
-        }else{
-            this.isDisabled = false;
-            this.form.StartTime='';
+        this.startTime = "";
+        this.endTime = "";
+        var newval = [];
+        if (val) {
+          var newval = val.model;
+          this.startTime = val.startTime;
+          this.endTime = val.endTime;
         }
+        let PositionName = newval.map(v => v.TeamName).join(",");
+        let UserName = "";
+        let Weather = "";
+        let Temperature = "";
+        let StartTime = "";
+
+        if (PositionName) {
+          UserName = newval[0].UserName;
+          Weather = newval[0].Weather;
+          Temperature = newval[0].Temperature;
+          StartTime = newval[0].StartTime;
+          // let endTime = this.endTime.split(" ");
+          // if (new Date() > new Date(endTime[0] + " " + endTime[3])) {
+          //   this.isDisabled = true;
+          //   this.isDisabled1 = false;
+          //   this.getCurrentDate1();
+          // } else {
+          //   this.isDisabled = false;
+          //   this.isDisabled1 = true;
+          //   this.getCurrentDate();
+          // }
+        }
+        this.reset({ PositionName, UserName, Weather, Temperature, StartTime });
+        this.reset1({ PositionName, UserName, StartTime });
       },
-      deep: true 
+      deep: true
     }
   },
   computed: {
     ...mapGetters({
       companyType: "status/companyType"
-    })
+    }),
+    isDisabled() {
+      if (this.form.PositionName) {
+        let endTime = this.endTime.split(" ");
+        if (new Date() < new Date(endTime[0] + " " + endTime[3])) {
+          this.getCurrentDate();
+          return true;
+        }
+      }
+      return false;
+    },
+    isDisabled1() {
+      if (this.form1.PositionName) {
+        let endTime = this.endTime.split(" ");
+        if (new Date() > new Date(endTime[0] + " " + endTime[3])) {
+          this.getCurrentDate1();
+          return true;
+        }
+      }
+      return false;
+    }
   },
   methods: {
     getCurrentDate() {
-      setTimeout(() => {
-        this.form.StartTime = new Date();
-        this.getCurrentDate();
-      }, 1000);
+      if (!this.isDisabled)
+        setTimeout(() => {
+          this.form.StartTime = new Date();
+          this.getCurrentDate();
+        }, 1000);
+      else this.form.StartTime = "";
     },
     getCurrentDate1() {
-      setTimeout(() => {
-        this.form1.StartTime = new Date();
-        this.getCurrentDate1();
-      }, 1000);
+      if (!this.isDisabled1)
+        setTimeout(() => {
+          this.form1.StartTime = new Date();
+          this.getCurrentDate1();
+        }, 1000);
+      else this.form1.StartTime = "";
     },
-    handleAccept() {},
+    handleAccept1() {
+      this.$refs.form1.validate(v => {
+        if (v) {
+          var newval = this.currentData.model;
+          let Ids = newval.map(v => v.Id);
+          let endtime = new Date();
+          let weather = this.form.Weather;
+          let temperature = this.form.Temperature;
+          updateHandover({
+            Ids,
+            endtime,
+            weather,
+            temperature
+          }).then(r => {
+            this.$message.success("交班成功");
+          });
+        }
+      });
+    },
+    handleAccept() {
+      this.$refs.form.validate(v => {
+        if (v) {
+          var newval = this.currentData.model;
+          let positionIds = newval.map(v => v.Id);
+          let dutyTeamId = newval[0].TeamId;
+          let starttime = new Date();
+          let weather = this.form.Weather;
+          let temperature = this.form.Temperature;
+          addHandover({
+            positionIds,
+            dutyTeamId,
+            starttime,
+            weather,
+            temperature
+          }).then(r => {
+            this.$message.success("接班成功");
+          });
+        }
+      });
+    },
     handleReset() {
       this.form.Temperature = "";
       this.form.Weather = "";
@@ -174,7 +275,7 @@ export default {
     reset(data) {
       this.form = Object.assign(
         {
-          StartTime: new Date(),
+          StartTime: "",
           UserName: "",
           Weather: "",
           Temperature: "",
@@ -182,11 +283,12 @@ export default {
         },
         data
       );
+      this.$refs.form.clearValidate();
     },
     reset1(data) {
       this.form1 = Object.assign(
         {
-          StartTime: new Date(),
+          StartTime: "",
           UserName: "",
           Weather: "",
           Temperature: "",
@@ -198,10 +300,10 @@ export default {
           Temperature: ""
         }
       );
+      this.$refs.form1.clearValidate();
     }
   }
 };
-
 </script>
 
 <style lang="scss" scoped>
@@ -258,7 +360,8 @@ export default {
   }
   label {
     color: #303133;
-    font-weight: normal;display: inline-block;
+    font-weight: normal;
+    display: inline-block;
     width: 193px;
     font-size: 14px;
     margin: 0 15px;
@@ -266,7 +369,7 @@ export default {
   &.left {
     right: -335px;
     top: 42%;
-   
+
     &::before {
       content: "";
       width: 70px;
@@ -288,8 +391,8 @@ export default {
       vertical-align: middle;
     }
   }
-  &.blue label{
-      color: #558cf7;
+  &.blue label {
+    color: #558cf7;
   }
 }
 .leftcard {
