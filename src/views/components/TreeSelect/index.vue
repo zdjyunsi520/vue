30242dfd39bfc2c58e98fe5933f1604630667156
zxl520 <764880808@qq.com>
2020-06-23 00:37:26 +0800

    123123

--- src/views/components/TreeSelect/index.vue
+++ src/views/components/TreeSelect/index.vue
@@ -1,108 +1,109 @@
 <template>
-  <div>
-    <el-button :disabled="disabled" placeholder="" @click="showTree">{{this.$refs.tree&&this.$refs.tree.getCheckedNodes(true).map(v => v[this.showText]).join(',')}}</el-button>
-    <el-drawer :wrapperClosable="false" :modal-append-to-body='true' :title="title" direction="rtl" :visible.sync="drawperdialogVisible" :show-close='false' center :size="size">
-      <el-scrollbar style="height: 86vh;" class="marginright-fx">
-        <el-tree :default-checked-keys="checkedKeys" ref="tree" :node-key="nodeKey" :default-expand-all="false" :props="props" :data="data" show-checkbox :check-strictly="!mutiple" @check-change="handleCheckChange"></el-tree>
-      </el-scrollbar>
-      <div class="dialog-footer">
-        <el-button type="primary" @click="handleConfirm">
-          <svg-icon icon-class='ic_save' class='tablesvgicon'></svg-icon>保 存
-        </el-button>
-        <el-button @click="handleCancel">取 消</el-button>
-      </div>
-    </el-drawer>
-  </div>
+    <div>
+        <el-button :disabled="disabled" placeholder="" @click="showTree">{{this.$refs.tree&&this.$refs.tree.getCheckedNodes(true).map(v => v[this.showText]).join(',')}}</el-button>
+        <el-drawer :wrapperClosable="false" :modal-append-to-body='true' :title="title" direction="rtl" :visible.sync="drawperdialogVisible" :show-close='false' center :size="size">
+            <el-scrollbar style="height: 86vh;" class="marginright-fx">
+                <el-tree :default-checked-keys="checkedKeys" ref="tree" :node-key="nodeKey" :default-expand-all="false" :props="props" :data="data" show-checkbox :check-strictly="!mutiple" @check-change="handleCheckChange"></el-tree>
+            </el-scrollbar>
+            <div class="dialog-footer">
+                <el-button type="primary" @click="handleConfirm">
+                    <svg-icon icon-class='ic_save' class='tablesvgicon'></svg-icon>保 存
+                </el-button>
+                <el-button @click="handleCancel">取 消</el-button>
+            </div>
+        </el-drawer>
+    </div>
 </template>
 
 <script>
 export default {
-  props: {
-    //数据源
-    data: {
-      type: Array
-    },
-    title: {
-      type: String,
-      default: "请选择"
-    },
     props: {
-      type: Object,
-      default() {
+        //数据源
+        data: {
+            type: Array
+        },
+        title: {
+            type: String,
+            default: "请选择"
+        },
+        props: {
+            type: Object,
+            default() {
+                return {
+                    label: "text",
+                    children: "childs"
+                };
+            }
+        },
+        nodeKey: {
+            type: String,
+            default: "id"
+        },
+        //多选
+        mutiple: {
+            type: Boolean,
+            default: true
+        },
+        checkedKeys: {
+            type: Array,
+            default() {
+                return [];
+            }
+        },
+        showText: {
+            type: String,
+            default: "text"
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
         return {
-          label: "text",
-          children: "childs"
+            drawperdialogVisible: true,
+            size: "0",
+            oldKey: []
         };
-      }
     },
-    nodeKey: {
-      type: String,
-      default: "id"
+    mounted() {
+        this.drawperdialogVisible = false;
+        setTimeout(() => {
+            this.size = "300px";
+        }, 1000);
     },
-    //多选
-    mutiple: {
-      type: Boolean,
-      default: true
-    },
-    checkedKeys: {
-      type: Array,
-      default() {
-        return [];
-      }
-    },
-    showText: {
-      type: String,
-      default: "text"
-    },
-    disabled: {
-      type: Boolean,
-      default: false
+    methods: {
+        showTree() {
+            this.drawperdialogVisible = true;
+            this.oldKey = this.$refs.tree.getCheckedNodes(true).map(v => v);
+        },
+        handleCheckChange(node, select) {
+            if (!this.mutiple && select) {
+                this.$refs.tree.setCheckedKeys([node.id]);
+            }
+            this.$emit("change", this.$refs.tree.getCheckedNodes(true));
+        },
+        handleClose() {
+            this.$refs.tree.setCheckedKeys(this.oldKey.map(v => v.id));
+            this.$emit("change", this.oldKey);
+            this.drawperdialogVisible = false;
+        },
+        handleConfirm() {
+            this.oldKey = this.$refs.tree.getCheckedNodes(true);
+            this.handleClose();
+        },
+        handleCancel() {
+            this.handleClose();
+        }
     }
-  },
-  data() {
-    return {
-      drawperdialogVisible: true,
-      size: "0",
-      oldKey: []
-    };
-  },
-  mounted() {
-    this.drawperdialogVisible = false;
-    setTimeout(() => {
-      this.size = "300px";
-    }, 1000);
-  },
-  methods: {
-    showTree() {
-      this.drawperdialogVisible = true;
-      this.oldKey = this.$refs.tree.getCheckedNodes(true).map(v => v);
-    },
-    handleCheckChange(node, select) {
-      if (!this.mutiple && select) {
-        this.$refs.tree.setCheckedKeys([node.id]);
-      }
-    },
-    handleClose() {
-      this.$refs.tree.setCheckedKeys(this.oldKey.map(v => v.id));
-      this.$emit("change", this.oldKey);
-      this.drawperdialogVisible = false;
-    },
-    handleConfirm() {
-      this.oldKey = this.$refs.tree.getCheckedNodes(true);
-      this.handleClose();
-    },
-    handleCancel() {
-      this.handleClose();
-    }
-  }
 };
 </script>
 
 <style lang="scss" scoped>
 .el-form-item__content {
-  .el-button {
-    width: 100%;
-    text-align: left;
-  }
+    .el-button {
+        width: 100%;
+        text-align: left;
+    }
 }
 </style>
