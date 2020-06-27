af8a4705aba99bf65f056b5086bc27aad3b82c80
zxl520 <764880808@qq.com>
2020-06-28 00:07:50 +0800

    1231312

--- src/views/equipmentAccount/components/index.vue
+++ src/views/equipmentAccount/components/index.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="app-container">
-    <el-row :gutter="20" class="comheight dragbox" ref="dragbox">
-      <el-col :xs="{ span: 24 }" class="treebox comheight dragleft">
-        <div style="background:#fff;height:100%;padding-left:10px;">
-          <el-scrollbar ref="elScrollbar" v-loading.fullscreen.lock="loading" element-loading-background="rgba(0, 0, 0, 0.4)" element-loading-text="Loading" element-loading-spinner="el-icon-loading">
-            <div style="padding-right:10px">
+    <div class="app-container">
+        <el-row :gutter="20" class="comheight dragbox" ref="dragbox">
+            <el-col :xs="{ span: 24 }" class="treebox comheight dragleft">
+                <div style="background:#fff;height:100%;padding-left:10px;">
+                    <el-scrollbar ref="elScrollbar" v-loading.fullscreen.lock="loading" element-loading-background="rgba(0, 0, 0, 0.4)" element-loading-text="Loading" element-loading-spinner="el-icon-loading">
+                        <div style="padding-right:10px">
 
-              <el-tree ref="tree" :current-node-key="currentNode[nodeKey]" :node-key="nodeKey" :data="treeData" :props="defaultProps" class="comheight" @node-click="handleNodeClick" :highlight-current="true" :default-expand-all="false" :expand-on-click-node="false" :default-expanded-keys="expandedKeys">
-                <span class="el-tree-node__label" slot-scope="{ node, data }">
-                  <svg-icon :icon-class="
+                            <el-tree ref="tree" :current-node-key="currentNode[nodeKey]" :node-key="nodeKey" :data="treeData" :props="defaultProps" class="comheight" @node-click="handleNodeClick" :highlight-current="true" :default-expand-all="false" :expand-on-click-node="false" :default-expanded-keys="expandedKeys">
+                                <span class="el-tree-node__label" slot-scope="{ node, data }">
+                                    <svg-icon :icon-class="
                       data.type == 1
                         ? 'gongsi'
                         : data.type == 2
@@ -33,164 +33,184 @@
                         ? 'jiange'
                         : ''
                   " class="tablesvgicon"></svg-icon>
-                  <!-- <span :class="data.type == 2 ? 'icon-ic_capacity' : ''"></span -->
-                  {{ data.text }}
-                </span>
-              </el-tree>
-            </div>
+                                    <!-- <span :class="data.type == 2 ? 'icon-ic_capacity' : ''"></span -->
+                                    {{ data.text }}
+                                </span>
+                            </el-tree>
+                        </div>
 
-          </el-scrollbar>
-        </div>
-      </el-col>
-      <el-col class="dragresize">
-        <span class="iconslider">
-          <svg-icon icon-class="ic_drag" style="font-size:26px;margin-left:-9px;" />
-          <i class="el-icon-arrow-left" style="font-size:12px;margin-left:-2px;" />
-        </span>
-      </el-col>
-      <el-col :xs="{ span: 24 }" class="comheight dragright">
-        <slot />
-      </el-col>
-    </el-row>
-  </div>
+                    </el-scrollbar>
+                </div>
+            </el-col>
+            <el-col class="dragresize">
+                <span class="iconslider">
+                    <svg-icon icon-class="ic_drag" style="font-size:26px;margin-left:-9px;" />
+                    <i class="el-icon-arrow-left" style="font-size:12px;margin-left:-2px;" />
+                </span>
+            </el-col>
+            <el-col :xs="{ span: 24 }" class="comheight dragright">
+                <slot />
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
 import { getTrees } from "@/api/org";
 export default {
-  props: {
-    expandedKeys: {
-      type: Array,
-      default() {
-        return [];
-      }
+    props: {
+        // expandedKeys: {
+        //   type: Array,
+        //   default() {
+        //     return [];
+        //   }
+        // },
+        nodeKey: {
+            type: String,
+            default: "id"
+        },
+        currentNode: {
+            type: Object,
+            default() {
+                return {};
+            }
+        },
+        needToScroll: {
+            type: Number,
+            default: 1
+        }
     },
-    nodeKey: {
-      type: String,
-      default: "id"
+    data() {
+        return {
+            treeData: [],
+            defaultProps: {
+                children: "childs",
+                label: "text"
+            },
+            dataArray: [],
+            count: 0,
+            loading: false,
+            expandedKeys: []
+        };
     },
-    currentNode: {
-      type: Object,
-      default() {
-        return {};
-      }
+    mounted() {
+        this.dragControllerDiv();
     },
-    needToScroll: {
-      type: Number,
-      default: 1
-    }
-  },
-  data() {
-    return {
-      treeData: [],
-      defaultProps: {
-        children: "childs",
-        label: "text"
-      },
-      dataArray: [],
-      count: 0,
-      loading: false
-    };
-  },
-  mounted() {
-    this.dragControllerDiv();
-  },
-  created() {
-    this.getTreeData();
-  },
-
-  methods: {
-    renderContent(h, { node, data, store }) {
-      return (
-        <span class={data.type > 1 ? "custom-tree-node" : ""}>
-          <span>
-            {data}
-            {data.type}
-          </span>
-        </span>
-      );
+    created() {
+        this.getTreeData();
     },
 
-    // 获取设备关系树状图
-    getTreeData() {
-      this.count = 0;
-      this.loading = true;
-      getTrees()
-        .then(response => {
-          this.treeData = response.data;
-          this.dataArray = [];
-          if (this.treeData && this.treeData.length) {
-            this.$emit("getInfo", this.treeData[0]);
-            this.treeData.forEach(v => {
-              this.dataArray.push(v[this.nodeKey]);
-              if (v.childs) {
-                v.childs.forEach(v => {
-                  this.dataArray.push(v[this.nodeKey]);
-                  if (v.childs) {
-                    v.childs.forEach(v => {
-                      this.dataArray.push(v[this.nodeKey]);
-                      if (v.childs) {
-                        v.childs.forEach(v => {
-                          this.dataArray.push(v[this.nodeKey]);
-                          if (v.childs) {
-                            v.childs.forEach(v => {
-                              this.dataArray.push(v[this.nodeKey]);
-                              if (v.childs) {
+    methods: {
+        renderContent(h, { node, data, store }) {
+            return (
+                <span class={data.type > 1 ? "custom-tree-node" : ""}>
+                    <span>
+                        {data}
+                        {data.type}
+                    </span>
+                </span>
+            );
+        },
+
+        // 获取设备关系树状图
+        getTreeData() {
+            this.count = 0;
+            this.loading = true;
+            getTrees()
+                .then(response => {
+                    this.treeData = response.data;
+                    this.dataArray = [];
+                    if (this.treeData && this.treeData.length) {
+                        this.$emit("getInfo", this.treeData[0]);
+                        this.treeData.forEach(v => {
+                            this.dataArray.push(v[this.nodeKey]);
+                            if (v.childs) {
                                 v.childs.forEach(v => {
-                                  this.dataArray.push(v[this.nodeKey]);
+                                    this.dataArray.push(v[this.nodeKey]);
+                                    if (v.childs) {
+                                        v.childs.forEach(v => {
+                                            this.dataArray.push(
+                                                v[this.nodeKey]
+                                            );
+                                            if (v.childs) {
+                                                v.childs.forEach(v => {
+                                                    this.dataArray.push(
+                                                        v[this.nodeKey]
+                                                    );
+                                                    if (v.childs) {
+                                                        v.childs.forEach(v => {
+                                                            this.dataArray.push(
+                                                                v[this.nodeKey]
+                                                            );
+                                                            if (v.childs) {
+                                                                v.childs.forEach(
+                                                                    v => {
+                                                                        this.dataArray.push(
+                                                                            v[
+                                                                                this
+                                                                                    .nodeKey
+                                                                            ]
+                                                                        );
+                                                                    }
+                                                                );
+                                                            }
+                                                        });
+                                                    }
+                                                });
+                                            }
+                                        });
+                                    }
                                 });
-                              }
-                            });
-                          }
+                            }
                         });
-                      }
-                    });
-                  }
+                        if (!this.currentNode[this.nodeKey]) {
+                            this.handleNodeClick(this.treeData[0]);
+                        } else {
+                            this.handleNodeClick(this.currentNode);
+                        }
+
+                        if (this.count++ < this.needToScroll) {
+                            this.$nextTick(() => {
+                                // const scrollTop =
+                                //   ((this.$refs.elScrollbar.$refs.wrap.offsetWidth - 20) / 32) *
+                                //   this.dataArray.findIndex(v => v == this.currentNode.id);
+                                let targetValue = this.currentNode[
+                                    this.nodeKey
+                                ];
+
+                                const nowIndex =
+                                    this.dataArray.findIndex(
+                                        v => v == targetValue
+                                    ) - 3;
+                                const scrollTop = 32 * nowIndex;
+                                // console.log(nowIndex);
+                                this.$refs.elScrollbar.$refs.wrap.scrollTop = scrollTop;
+                            });
+                        }
+                    }
+                })
+                .finally(() => (this.loading = false));
+        },
+        handleNodeClick(node, event) {
+            if (node[this.nodeKey]) {
+                this.$nextTick(() => {
+                    let id = node[this.nodeKey] || node.Id;
+                    this.$refs.tree.setCurrentKey(id);
+                    this.expandedKeys.push(id);
                 });
-              }
-            });
-            if (!this.currentNode[this.nodeKey]) {
-              this.handleNodeClick(this.treeData[0]);
-            } else {
-              this.handleNodeClick(this.currentNode);
+                const id = node.id;
+                const type = node.type;
+                console.log(id);
+                this.$emit("getInfo", { id, type });
             }
-
-            if (this.count++ < this.needToScroll) {
-              this.$nextTick(() => {
-                // const scrollTop =
-                //   ((this.$refs.elScrollbar.$refs.wrap.offsetWidth - 20) / 32) *
-                //   this.dataArray.findIndex(v => v == this.currentNode.id);
-                let targetValue = this.currentNode[this.nodeKey];
-
-                const nowIndex =
-                  this.dataArray.findIndex(v => v == targetValue) - 3;
-                const scrollTop = 32 * nowIndex;
-                // console.log(nowIndex);
-                this.$refs.elScrollbar.$refs.wrap.scrollTop = scrollTop;
-              });
-            }
-          }
-        })
-        .finally(() => (this.loading = false));
-    },
-    handleNodeClick(node, event) {
-      if (node[this.nodeKey]) {
-        this.$nextTick(() => {
-          this.$refs.tree.setCurrentKey(node[this.nodeKey]);
-        });
-        const id = node.id;
-        const type = node.type;
-        console.log(id);
-        this.$emit("getInfo", { id, type });
-      }
+        }
     }
-  }
 };
 </script>
 
 <style lang="scss" scoped>
 @import "../../../styles/treeEquipment.scss";
 .dragbox .dragleft {
-  width: 324px;
+    width: 324px;
 }
 </style>
--- src/views/equipmentAccount/maintain/index.vue
+++ src/views/equipmentAccount/maintain/index.vue
@@ -1,18 +1,18 @@
 <template>
 
-  <common-tree @getInfo="getInfo" ref="commonTree" :currentNode="currentNode" :needToScroll="needToScroll" :expandedKeys="expandedKeys">
-    <div class="comheight">
-      <base-prop ref="component1" />
-      <power-room ref="component2" @refresh="refresh" />
-      <panel-cabinet ref="component3" @refresh="refresh" />
-      <camera ref="component8" @refresh="refresh" />
-      <interval ref="component11" @refresh="refresh" />
-      <communication-host ref="component4" @refresh="refresh" />
-      <smoke ref="component7" @refresh="refresh" />
-      <temperature ref="component6" @refresh="refresh" />
-      <clock ref="component5" @refresh="refresh" />
-    </div>
-  </common-tree>
+    <common-tree @getInfo="getInfo" ref="commonTree" :currentNode="currentNode" :needToScroll="needToScroll" :expandedKeys="expandedKeys">
+        <div class="comheight">
+            <base-prop ref="component1" />
+            <power-room ref="component2" @refresh="refresh" />
+            <panel-cabinet ref="component3" @refresh="refresh" />
+            <camera ref="component8" @refresh="refresh" />
+            <interval ref="component11" @refresh="refresh" />
+            <communication-host ref="component4" @refresh="refresh" />
+            <smoke ref="component7" @refresh="refresh" />
+            <temperature ref="component6" @refresh="refresh" />
+            <clock ref="component5" @refresh="refresh" />
+        </div>
+    </common-tree>
 
 </template>
 
@@ -29,68 +29,67 @@ import smoke from "./smoke";
 import temperature from "./temperature";
 import clock from "./clock";
 export default {
-  components: {
-    commonTree,
-    baseProp,
-    powerRoom,
-    panelCabinet,
-    camera,
-    interval,
-    communicationHost,
-    smoke,
-    temperature,
-    clock
-  },
-  data() {
-    return {
-      operateId: "",
-      loading: true,
-      currentNode: {},
-      needToScroll: 1,
-      expandedKeys: []
-    };
-  },
+    components: {
+        commonTree,
+        baseProp,
+        powerRoom,
+        panelCabinet,
+        camera,
+        interval,
+        communicationHost,
+        smoke,
+        temperature,
+        clock
+    },
+    data() {
+        return {
+            operateId: "",
+            loading: true,
+            currentNode: {},
+            needToScroll: 1,
+            expandedKeys: []
+        };
+    },
 
-  created() {
-    const { data } = this.$route.params;
-    this.currentNode = data || {};
-    if (data) {
-      this.expandedKeys.push(this.currentNode.tenantId);
+    created() {
+        const { data } = this.$route.params;
+        this.currentNode = data || {};
+        let id = this.currentNode.id || this.currentNode.Id;
+        this.currentNode.id = id;
+        if (data) {
+            this.expandedKeys.push(this.currentNode.tenantId);
+        }
+    },
+    methods: {
+        closeComponent() {
+            [1, 2, 3, 8, 11, 4, 7, 6, 5].forEach(v => {
+                const target = this.$refs["component" + v];
+                if (target) target.visible = false;
+            });
+        },
+        getInfo(data) {
+            this.data = data;
+            this.closeComponent();
+            const target = this.$refs["component" + data.type];
+            if (target) {
+                target.visible = true;
+                target.showBtn = true;
+                target.infoData = {};
+                target.getInfo(data);
+            }
+        },
+        handleCommand(commond) {
+            if (commond == "a") {
+                this.$router.push({ path: "", params: {} });
+            }
+        },
+        refresh() {
+            this.currentNode = {};
+            this.$refs.commonTree.getTreeData();
+        },
+        handleUpdate() {},
+        handleDelete() {}
     }
-    console.log(33333, this.currentNode);
-  },
-  methods: {
-    closeComponent() {
-      [1, 2, 3, 8, 11, 4, 7, 6, 5].forEach(v => {
-        const target = this.$refs["component" + v];
-        if (target) target.visible = false;
-      });
-    },
-    getInfo(data) {
-      this.data = data;
-      console.log("aaa", data.type);
-      this.closeComponent();
-      const target = this.$refs["component" + data.type];
-      if (target) {
-        console.log("bbb");
-        target.visible = true;
-        target.showBtn = true;
-        target.infoData = {};
-        target.getInfo(data);
-      }
-    },
-    handleCommand(commond) {
-      if (commond == "a") {
-        this.$router.push({ path: "", params: {} });
-      }
-    },
-    refresh() {
-      this.currentNode = {};
-      this.$refs.commonTree.getTreeData();
-    },
-    handleUpdate() {},
-    handleDelete() {}
-  }
 };
 </script>
 
